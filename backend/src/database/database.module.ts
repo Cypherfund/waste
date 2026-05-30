4bcec8be722383e27d5af0facf02c6d35df5fecf
Ngai Elizabeth <elizabeth@hivecpq.com>
2026-05-30 08:17:32 +0100

    Re-enable synchronize for test environment to work with migration-based schema

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -15,7 +15,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         password: configService.get<string>('database.password'),
         database: configService.get<string>('database.name'),
         autoLoadEntities: true,
-        synchronize: configService.get<string>('nodeEnv') === 'development',
+        synchronize:
+          configService.get<string>('nodeEnv') === 'development' ||
+          configService.get<string>('nodeEnv') === 'test',
         logging:
           configService.get<string>('nodeEnv') === 'development' ||
           configService.get<string>('nodeEnv') === 'test'
